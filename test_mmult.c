fbe3c719d3ec0402598f450a22012758007025ab
Thomas H McLaughlin <tuh42312@chinook.cis.temple.edu>
2021-03-09 18:31:55 -0500

    Changed formatting of the output, wrote to files, cleaned up files. The mmult and mmult_simd should be ready to plot. We need to change the make file to include the optimization in the compilation of mmult_test_simd, maybe with a new make section called simd_o3_test.

--- test_mmult.c
+++ test_mmult.c
@@ -16,7 +16,7 @@ int test_unoptimized(double *a, int arows, int acols,
         mmult(c_calc, a, i, i, b, i, i);
         end = clock();
         double time_taken = ((double)(end - start))/CLOCKS_PER_SEC;
-        printf("matrix size %i - %f seconds taken\n", i, time_taken);
+        printf("%i,%f\n", i, time_taken);
     }
     int are_same = compare_matrices(c_actual, c_calc, MAT_SIZE, MAT_SIZE);
     free(c_calc);
