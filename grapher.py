6ab32bdd2cb848a4b221e433891f860814b62e08
Seanwilliams2 <tui27140@temple.edu>
2021-03-10 16:36:56 -0500

    added more automation so that there is no need for command line arguments. As long as the file ends with STM and is in the current directory then it will graph the data

--- grapher.py
+++ grapher.py
@@ -3,21 +3,27 @@
 import sys
 import matplotlib.pyplot as plt 
 from itertools import cycle
-
-colors = ['g','c', 'r', 'b', 'm', 'y', 'k']
+import os
+colors = ['g','r', 'b', 'c', 'm', 'y', 'k']
 
 y_array = []
 x_array = []
-
+"""
 if(len(sys.argv) != 2 + int(sys.argv[1])):
     print("Not correct amount of arguments")
     exit(0)
+"""
 
-
-for i in range(2,int(sys.argv[1]) + 2):
+directory = os.listdir(os.getcwd())
+data = []
+for file1 in directory:
+    if "STM" in file1:
+        data.append(file1)
+count = 0
+for i in data:
     x_values = []
     y_values = []
-    with open(sys.argv[i], "r") as file1:
+    with open(i, "r") as file1:
          f_list = [float(i) for line in file1 for i in line.split(',') if i.strip()]
 
     for index, element in enumerate(f_list):
@@ -27,8 +33,8 @@ for i in range(2,int(sys.argv[1]) + 2):
             y_values.append(element)
     x_array.append(x_values)
     y_array.append(y_values)
-    plt.plot(x_array[i-2],y_array[i-2],c=colors[i%len(colors)],label=sys.argv[i])  
-    
+    plt.plot(x_array[count],y_array[count],c=colors[count%len(colors)],label=i)  
+    count += 1
 
 # ./grapher 3 input.txt input2.txt input3.txt
 
