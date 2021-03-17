ac20465540e21593060ca51b21079e3ca580287d
Seanwilliams2 <tui27140@temple.edu>
2021-03-17 13:49:13 -0400

    trying again

--- mpi_mmult.c
+++ mpi_mmult.c
@@ -1,95 +1,103 @@
-#include "mpi.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include "mpi.h"
+#include <time.h>
+#include <sys/time.h>
 
-#define MATSIZE 5
-#define aRows MATSIZE            
-#define bRows MATSIZE            
-#define bCols MATSIZE            
-#define MASTER 0               
-#define FROM_MASTER 1          
-#define FROM_WORKER 2          
+#define N 4
 
-int main (int argc, char *argv[])
-{
-int	numtasks = aRows;              
-int taskid;                
-int numworkers;            
-int source;            
-int dest;                 
-int mtype;             
-int rows;              
-int averow;
-int extra;
-int offset; 
-int i, j, k, rc;            
 MPI_Status status;
 
-MPI_Init(&argc,&argv);
-MPI_Comm_rank(MPI_COMM_WORLD,&taskid);
-MPI_Comm_size(MPI_COMM_WORLD,&numtasks);
+double a[N][N],b[N][N],c[N][N];
 
-numworkers = numtasks-1;
+int main(int argc, char **argv)
+{
+  int processCount, processId, slaveTaskCount, source, dest, rows, offset;
 
-double *a = read_matrix_from_file("a.txt");
-double *b = read_matrix_from_file("b.txt");
-double *c_actual = read_matrix_from_file("c.txt");
-double *c = malloc(MATSIZE * MATSIZE * sizeof(double));
-c = (double*)malloc(sizeof(double) * 5);
-   if (taskid == MASTER)
-   {
-      /* Measure start time */
-      double start = MPI_Wtime();
+  struct timeval start, stop;
 
-      averow = aRows/numworkers;
-      extra = aRows%numworkers;
-      offset = 0;
-      mtype = FROM_MASTER;
-      for (dest=1; dest<=numworkers; dest++)
-      {
-         rows = (dest <= extra) ? averow+1 : averow;   	
-         MPI_Send(&offset, 1, MPI_INT, dest, mtype, MPI_COMM_WORLD);
-         MPI_Send(&rows, 1, MPI_INT, dest, mtype, MPI_COMM_WORLD);
-         MPI_Send(&a, rows*bRows, MPI_DOUBLE, dest, mtype,
-                   MPI_COMM_WORLD);
-         MPI_Send(&b, bRows*bCols, MPI_DOUBLE, dest, mtype, MPI_COMM_WORLD);
-         offset = offset + rows;
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &processId);
+  MPI_Comm_size(MPI_COMM_WORLD, &processCount);
+
+  slaveTaskCount = processCount - 1;
+
+ if (processId == 0) {
+	
+    srand ( time(NULL) );
+    for (int i = 0; i<N; i++) {
+      for (int j = 0; j<N; j++) {
+        matrix_a[i][j]= rand()%10;
+        matrix_b[i][j]= rand()%10;
       }
+    }
+	
+  printf("\n\t\tMatrix - Matrix Multiplication using MPI\n");
 
-      mtype = FROM_WORKER;
-      for (i=1; i<=numworkers; i++)
-      {
-         source = i;
-         MPI_Recv(&offset, 1, MPI_INT, source, mtype, MPI_COMM_WORLD, &status);
-         MPI_Recv(&rows, 1, MPI_INT, source, mtype, MPI_COMM_WORLD, &status);
-         MPI_Recv(&c, rows*bCols, MPI_DOUBLE, source, mtype, 
-                  MPI_COMM_WORLD, &status);
+   printf("\nMatrix A\n\n");
+    for (int i = 0; i<N; i++) {
+      for (int j = 0; j<N; j++) {
+        printf("%.0f\t", matrix_a[i][j]);
       }
+	    printf("\n");
+    }
 
-      double finish = MPI_Wtime();
-      printf("Done in %f seconds.\n", finish - start);
-   }
+    printf("\nMatrix B\n\n");
+    for (int i = 0; i<N; i++) {
+      for (int j = 0; j<N; j++) {
+        printf("%.0f\t", matrix_b[i][j]);
+      }
+	    printf("\n");
+    }
 
+    rows = N/slaveTaskCount;
+    offset = 0;
 
-   if (taskid > MASTER)
-   {
-      mtype = FROM_MASTER;
-      MPI_Recv(&offset, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD, &status);
-      MPI_Recv(&rows, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD, &status);
-      MPI_Recv(&a, rows*bRows, MPI_DOUBLE, MASTER, mtype, MPI_COMM_WORLD, &status);
-      MPI_Recv(&b, bRows*bCols, MPI_DOUBLE, MASTER, mtype, MPI_COMM_WORLD, &status);
+    for (dest=1; dest <= slaveTaskCount; dest++)
+    {
+      MPI_Send(&offset, 1, MPI_INT, dest, 1, MPI_COMM_WORLD);
+      MPI_Send(&rows, 1, MPI_INT, dest, 1, MPI_COMM_WORLD);
+      MPI_Send(&matrix_a[offset][0], rows*N, MPI_DOUBLE,dest,1, MPI_COMM_WORLD);
+      MPI_Send(&matrix_b, N*N, MPI_DOUBLE, dest, 1, MPI_COMM_WORLD);
+      offset = offset + rows;
+    }
 
-      for (k=0; k<bCols; k++)
-         for (i=0; i<rows; i++)
-         {
-            c[i * bCols + j] = 0;
-            for (j=0; j<bRows; j++)
-               c[i * bCols + j] += a[i * aRows + k] * b[k * bCols + j];
-         }
-      mtype = FROM_WORKER;
-      MPI_Send(&offset, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD);
-      MPI_Send(&rows, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD);
-      MPI_Send(&c, rows*bCols, MPI_DOUBLE, MASTER, mtype, MPI_COMM_WORLD);
-   }
-   MPI_Finalize();
+    for (int i = 1; i <= slaveTaskCount; i++)
+    {
+      source = i;
+      MPI_Recv(&offset, 1, MPI_INT, source, 2, MPI_COMM_WORLD, &status);
+      MPI_Recv(&rows, 1, MPI_INT, source, 2, MPI_COMM_WORLD, &status);
+      MPI_Recv(&matrix_c[offset][0], rows*N, MPI_DOUBLE, source, 2, MPI_COMM_WORLD, &status);
+    }
+
+    printf("\nResult Matrix C = Matrix A * Matrix B:\n\n");
+    for (int i = 0; i<N; i++) {
+      for (int j = 0; j<N; j++)
+        printf("%.0f\t", matrix_c[i][j]);
+      printf ("\n");
+    }
+    printf ("\n");
+  }
+ //worker process
+  if (processId > 0) {
+    source = 0;
+    MPI_Recv(&offset, 1, MPI_INT, source, 1, MPI_COMM_WORLD, &status);
+    MPI_Recv(&rows, 1, MPI_INT, source, 1, MPI_COMM_WORLD, &status);
+    MPI_Recv(&matrix_a, rows*N, MPI_DOUBLE, source, 1, MPI_COMM_WORLD, &status);
+    MPI_Recv(&matrix_b, N*N, MPI_DOUBLE, source, 1, MPI_COMM_WORLD, &status);
+
+    for (int k = 0; k<N; k++) {
+      for (int i = 0; i<rows; i++) {
+        matrix_c[i][k] = 0.0;
+        for (int j = 0; j<N; j++)
+          matrix_c[i][k] = matrix_c[i][k] + matrix_a[i][j] * matrix_b[j][k];
+      }
+    }
+
+    MPI_Send(&offset, 1, MPI_INT, 0, 2, MPI_COMM_WORLD);
+    MPI_Send(&rows, 1, MPI_INT, 0, 2, MPI_COMM_WORLD);
+    MPI_Send(&matrix_c, rows*N, MPI_DOUBLE, 0, 2, MPI_COMM_WORLD);
+  }
+
+  MPI_Finalize();
 }
