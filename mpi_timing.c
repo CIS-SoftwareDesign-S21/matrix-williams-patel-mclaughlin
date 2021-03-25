58abb03fe2a0d6314c40ca73c903ba2510dd6eea
Seanwilliams2 <tui27140@temple.edu>
2021-03-25 19:07:31 -0400

    testing with changing array

--- mpi_timing.c
+++ mpi_timing.c
@@ -12,17 +12,18 @@ int main(int argc, char **argv)
   struct timeval t0,t1;
   pid_t child_pid;
   int child_status;
-  char* args[5] = {"mpiexec", "-n", "4","./test_mpi2","10",NULL};
+  char* args[6] = {"mpiexec", "-n", "4","./test_mpi2"};
   
   pid_t pid;
-//for(int i = 0; i < atoi(argv[1]); i++){
+for(int i = 0; i < atoi(argv[1]); i++){
     if (fork()== 0){
-            args[4] = "15";
+            args[4] = i +'0';
+            args[5] = NULL;
             execvp(args[0], args);
         } else {
             waitpid(pid, &status, 0);
         }
-//}
+}
     
         
     
