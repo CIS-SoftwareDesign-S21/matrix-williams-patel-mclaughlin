40450a904701242e96940fd15dff416fb3ed15c7
Seanwilliams2 <tui27140@temple.edu>
2021-03-25 17:01:02 -0400

    changed timing back to mpi

--- mpi_mmult.c
+++ mpi_mmult.c
@@ -14,8 +14,8 @@ MPI_Status status;
 int main(int argc, char **argv)
 {
   int processCount, processId, workerTaskCount, source, dest, rows, offset, extra;
-  int N = 0;
-  double starttime, endtime;
+  int N,runs = 0;
+  double starttime, endtime, totaltime, worktime;
 
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &processId);
@@ -23,7 +23,7 @@ int main(int argc, char **argv)
 
   workerTaskCount = processCount - 1;
   if (argc > 1) {
-    N = atoi(argv[1]);
+    runs = atoi(argv[1]);
   }
   /*
   if(N % workerTaskCount != 0 || N == 0){ //if the matrix size is 0 or the amount of rows/cols isnt divisable by the number of workers then the program aborts
@@ -31,100 +31,106 @@ int main(int argc, char **argv)
     MPI_Abort(MPI_COMM_WORLD,1);
   }
   */
-double a[N][N],b[N][N],c[N][N]; //creating space for the matricies
+for(N = 0; N < runs; N++)
+{
 
- if (processId == 0) {
-	//creates two matricies with random values a and b
-    srand ( time(NULL) );
-    for (int i = 0; i<N; i++) {
-      for (int j = 0; j<N; j++) {
-        a[i][j]= rand()%10;
-        b[i][j]= rand()%10;
-      }
-    }
-/*
-   printf("\nMatrix A\n\n");
-    for (int i = 0; i<N; i++) {
-      for (int j = 0; j<N; j++) {
-        printf("%.0f\t", a[i][j]);
-      }
-	    printf("\n");
-    }
+  double a[N][N],b[N][N],c[N][N]; //creating space for the matricies
 
-    printf("\nMatrix B\n\n");
-    for (int i = 0; i<N; i++) {
-      for (int j = 0; j<N; j++) {
-        printf("%.0f\t", b[i][j]);
+  if (processId == 0) {
+    //creates two matricies with random values a and b
+      srand ( time(NULL) );
+      for (int i = 0; i<N; i++) {
+        for (int j = 0; j<N; j++) {
+          a[i][j]= rand()%10;
+          b[i][j]= rand()%10;
+        }
       }
-	    printf("\n");
-    }
-*/
-    starttime = MPI_Wtime();
-    rows = N/workerTaskCount; //number of rows to be sent to the child processes
-    extra = N%workerTaskCount;
-    offset = 0; //how we determine which row(s) we sending to which child process
+  /*
+    printf("\nMatrix A\n\n");
+      for (int i = 0; i<N; i++) {
+        for (int j = 0; j<N; j++) {
+          printf("%.0f\t", a[i][j]);
+        }
+        printf("\n");
+      }
 
-    for (dest=1; dest <= workerTaskCount; dest++)
-    {
-      //Offset, number of rows, the row of A to be computed, and matrix b are sent to a child process
-      MPI_Send(&offset, 1, MPI_INT, dest, 1, MPI_COMM_WORLD);
-      MPI_Send(&rows, 1, MPI_INT, dest, 1, MPI_COMM_WORLD);
-      MPI_Send(&a[offset][0], rows*N, MPI_DOUBLE,dest,1, MPI_COMM_WORLD);
-      MPI_Send(&b, N*N, MPI_DOUBLE, dest, 1, MPI_COMM_WORLD);
-      offset = offset + rows; //increment offset so we aren't sending the same rows
-    }
-    if(offset < N) //compute extra rows if there is a modulus 
-    {
-      for (int k = 0; k<N; k++) {
-      for (int i = offset; i<N; i++) {
-        c[i][k] = 0.0;
+      printf("\nMatrix B\n\n");
+      for (int i = 0; i<N; i++) {
+        for (int j = 0; j<N; j++) {
+          printf("%.0f\t", b[i][j]);
+        }
+        printf("\n");
+      }
+  */
+
+      starttime = MPI_Wtime();
+      rows = N/workerTaskCount; //number of rows to be sent to the child processes
+      extra = N%workerTaskCount;
+      offset = 0; //how we determine which row(s) we sending to which child process
+
+      for (dest=1; dest <= workerTaskCount; dest++)
+      {
+        //Offset, number of rows, the row of A to be computed, and matrix b are sent to a child process
+        MPI_Send(&offset, 1, MPI_INT, dest, 1, MPI_COMM_WORLD);
+        MPI_Send(&rows, 1, MPI_INT, dest, 1, MPI_COMM_WORLD);
+        MPI_Send(&a[offset][0], rows*N, MPI_DOUBLE,dest,1, MPI_COMM_WORLD);
+        MPI_Send(&b, N*N, MPI_DOUBLE, dest, 1, MPI_COMM_WORLD);
+        offset = offset + rows; //increment offset so we aren't sending the same rows
+      }
+      if(offset < N) //compute extra rows if there is a modulus 
+      {
+        for (int k = 0; k<N; k++) {
+        for (int i = offset; i<N; i++) {
+          c[i][k] = 0.0;
+          for (int j = 0; j<N; j++)
+            c[i][k] = c[i][k] + a[i][j] * b[j][k];
+        }
+      }
+      }
+      for (int i = 1; i <= workerTaskCount; i++)
+      {
+        //here we recieve the rows computed by the workers
+        source = i;
+        MPI_Recv(&offset, 1, MPI_INT, source, 2, MPI_COMM_WORLD, &status);
+        MPI_Recv(&rows, 1, MPI_INT, source, 2, MPI_COMM_WORLD, &status);
+        MPI_Recv(&c[offset][0], rows*N, MPI_DOUBLE, source, 2, MPI_COMM_WORLD, &status);
+      }
+
+  /*
+      printf("\nResult Matrix C = Matrix A * Matrix B:\n\n");
+      for (int i = 0; i<N; i++) {
         for (int j = 0; j<N; j++)
-          c[i][k] = c[i][k] + a[i][j] * b[j][k];
+          printf("%.0f\t", c[i][j]);
+        printf ("\n");
       }
-    }
-    }
-    for (int i = 1; i <= workerTaskCount; i++)
-    {
-      //here we recieve the rows computed by the workers
-      source = i;
-      MPI_Recv(&offset, 1, MPI_INT, source, 2, MPI_COMM_WORLD, &status);
-      MPI_Recv(&rows, 1, MPI_INT, source, 2, MPI_COMM_WORLD, &status);
-      MPI_Recv(&c[offset][0], rows*N, MPI_DOUBLE, source, 2, MPI_COMM_WORLD, &status);
-    }
-
-/*
-    printf("\nResult Matrix C = Matrix A * Matrix B:\n\n");
-    for (int i = 0; i<N; i++) {
-      for (int j = 0; j<N; j++)
-        printf("%.0f\t", c[i][j]);
       printf ("\n");
-    }
-    printf ("\n");
-  
-*/
-  endtime = MPI_Wtime();
-  printf("Time taken: %f\n",(endtime - starttime));
- //worker process
- }
-  if (processId > 0) {
-    source = 0;
-    MPI_Recv(&offset, 1, MPI_INT, source, 1, MPI_COMM_WORLD, &status);
-    MPI_Recv(&rows, 1, MPI_INT, source, 1, MPI_COMM_WORLD, &status);
-    MPI_Recv(&a, rows*N, MPI_DOUBLE, source, 1, MPI_COMM_WORLD, &status);
-    MPI_Recv(&b, N*N, MPI_DOUBLE, source, 1, MPI_COMM_WORLD, &status);
-
-    for (int k = 0; k<N; k++) {
-      for (int i = 0; i<rows; i++) {
-        c[i][k] = 0.0;
-        for (int j = 0; j<N; j++)
-          c[i][k] = c[i][k] + a[i][j] * b[j][k];
-      }
-    }
-    MPI_Send(&offset, 1, MPI_INT, 0, 2, MPI_COMM_WORLD);
-    MPI_Send(&rows, 1, MPI_INT, 0, 2, MPI_COMM_WORLD);
-    MPI_Send(&c, rows*N, MPI_DOUBLE, 0, 2, MPI_COMM_WORLD);
-    //sends the computed rows back to the root 
+    
+  */
+    endTime = MPI_Wtime();
+    totalTime = endTime - startTime;
+    MPI_Reduce (&totalTime, &workTime, 1, MPI_DOUBLE_PRECISION, MPI_SUM, root, MPI_COMM_WORLD);
+    printf("Time taken: %f\n",(worktime/processCount);
+  //worker process
   }
+    if (processId > 0) {
+      source = 0;
+      MPI_Recv(&offset, 1, MPI_INT, source, 1, MPI_COMM_WORLD, &status);
+      MPI_Recv(&rows, 1, MPI_INT, source, 1, MPI_COMM_WORLD, &status);
+      MPI_Recv(&a, rows*N, MPI_DOUBLE, source, 1, MPI_COMM_WORLD, &status);
+      MPI_Recv(&b, N*N, MPI_DOUBLE, source, 1, MPI_COMM_WORLD, &status);
 
-  MPI_Finalize();
+      for (int k = 0; k<N; k++) {
+        for (int i = 0; i<rows; i++) {
+          c[i][k] = 0.0;
+          for (int j = 0; j<N; j++)
+            c[i][k] = c[i][k] + a[i][j] * b[j][k];
+        }
+      }
+      MPI_Send(&offset, 1, MPI_INT, 0, 2, MPI_COMM_WORLD);
+      MPI_Send(&rows, 1, MPI_INT, 0, 2, MPI_COMM_WORLD);
+      MPI_Send(&c, rows*N, MPI_DOUBLE, 0, 2, MPI_COMM_WORLD);
+      //sends the computed rows back to the root 
+    }
+}
+    MPI_Finalize();
 }
