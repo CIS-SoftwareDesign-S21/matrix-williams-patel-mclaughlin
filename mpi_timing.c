85154a8e1b283cf7bd88fda42e577243300997cf
Seanwilliams2 <tui27140@temple.edu>
2021-03-25 19:00:54 -0400

    test

--- mpi_timing.c
+++ mpi_timing.c
@@ -15,14 +15,14 @@ int main(int argc, char **argv)
   char* args[5] = {"mpiexec", "-n", "4","./test_mpi2","10",NULL};
   
   pid_t pid;
-for(int i = 0; i < atoi(argv[1]); i++){
+//for(int i = 0; i < atoi(argv[1]); i++){
     if (fork()== 0){
-            args[4] = i +'0';
+            args[4] = "15";
             execvp(args[0], args);
         } else {
             waitpid(pid, &status, 0);
         }
-}
+//}
     
         
     
