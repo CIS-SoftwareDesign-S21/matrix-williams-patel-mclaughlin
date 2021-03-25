2426de12feabea5a1cf3aebc3010bd6c0a68dc85
Seanwilliams2 <tui27140@temple.edu>
2021-03-25 18:58:40 -0400

    wrong argument

--- mpi_timing.c
+++ mpi_timing.c
@@ -15,7 +15,7 @@ int main(int argc, char **argv)
   char* args[5] = {"mpiexec", "-n", "4","./test_mpi2","10",NULL};
   
   pid_t pid;
-for(int i = 0; i < argv[1]; i++){
+for(int i = 0; i < atoi(argv[1]); i++){
     if (fork()== 0){
             args[4] = i +'0';
             execvp(args[0], args);
