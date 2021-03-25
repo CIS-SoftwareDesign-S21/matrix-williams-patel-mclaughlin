050d448f0ee2deda93c8ca06ea7b83021eb16cf4
Seanwilliams2 <tui27140@temple.edu>
2021-03-25 17:25:59 -0400

    timing

--- mpi_timing.c
+++ mpi_timing.c
@@ -12,11 +12,8 @@ int main(int argc, char **argv)
   struct timeval t0,t1;
   pid_t child_pid;
   int child_status;
-  char* args[8] = {"mpiexec", "-f", "~/hosts", "-n", "4", "./test_mpi2"};
-  if (argc > 1) {
-    args[6] = argv[1];
-    args[7] = NULL;
-  }
+  char* args[8] = {"mpiexec", "./test_mpi2","-f", "~/hosts", "-n", "4","10",NULL};
+  
   pid_t pid;
     if (fork()== 0){
         execvp(args[0], args);
